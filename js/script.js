6f8b0f64e4abd9d94a1ea7d2f59e2af1c1265c6d
Wojciech Lysien <wojciechlysien@gmail.com>
2019-11-03 14:50:23 +0000

    add game playing with result

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+function printMessage(msg) {
+  var div = document.createElement('div');
+  div.innerHTML = msg;
+  document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages() {
+  document.getElementById('messages').innerHTML = '';
+}
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+  computerMove = 'kamień';
+}
+else if (randomNumber == 2) {
+  computerMove = 'papier';
+} else if (randomNumber == 3) {
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -6,9 +33,17 @@ let playerMove = 'nieznany ruch';
 
 if (playerInput == '1') {
   playerMove = 'kamień';
-}
-else if (playerInput == '2') {
-  computerMove = 'papier';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nozyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (playerMove === computerMove) {
+  printMessage('Remis')
+} else if ((playerMove === 'Papier' && computerMove === 'kamien') || (playerMove === 'kamien' && computerMove === 'nozyczki') || (playerMove === 'nozyczki' && computerMove === 'Papier')) {
+  printMessage('Wygrales')
+} else printMessage('Przegrales')
+
