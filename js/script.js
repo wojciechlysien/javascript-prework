3bf8aca95330c44f40d5de3c9c6b51d340f163a4
Wojciech Lysien <wojciechlysien@gmail.com>
2019-11-05 09:53:03 +0000

    delleted

--- js/script.js
+++ js/script.js
@@ -9,9 +9,7 @@ function playGame(playerInput) {
 
   let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-  console.log('Wylosowana liczba to: ' + randomNumber);
 
-  // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
   let playerMove = getMoveName(playerInput);
   let computerMove = getMoveName(randomNumber);
 
@@ -34,32 +32,7 @@ function playGame(playerInput) {
     }
   }
 
-
-  console.log(computerMove)
-  // if (randomNumber == 1) {
-  //   computerMove = 'kamień';
-  // }
-  // else if (randomNumber == 2) {
-  //   computerMove = 'papier';
-  // } else if (randomNumber == 3) {
-  //   computerMove = 'nożyce';
-  // }
-
   printMessage('Mój ruch to: ' + computerMove);
-
-  // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-  console.log('Gracz wpisał: ' + playerInput);
-
-
-  console.log(playerMove)
-  // if (playerInput == '1') {
-  //   playerMove = 'kamień';
-  // } else if (playerInput == '2') {
-  //   playerMove = 'papier';
-  // } else if (playerInput == '3') {
-  //   playerMove = 'nozyce';
-  // }
 
   printMessage('Twój ruch to: ' + playerMove);
 
