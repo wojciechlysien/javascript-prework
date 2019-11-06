a3624c97c5ebc56f90824ec75a7a55bc4ec3d271
Wojciech Lysien <wojciechlysien@gmail.com>
2019-11-06 21:39:52 +0000

    improvement of the project code

--- js/script.js
+++ js/script.js
@@ -1,71 +1,73 @@
 
-const game = {
-  playerMove: '',
-  computerMove: '',
-}
-
-
-const hands = [...document.querySelectorAll('.select img')];
-
-function handSelection() {
-  game.playerMove = this.dataset.option;
-  hands.forEach(hand => hand.style.boxShadow = '');
-  this.style.boxShadow = '0 0 0 4px green';
-}
-
-
-function playGame(playerInput) {
-  if (!game.playerMove) {
-    return alert('Wybierz dłoń!!!')
+{
+  const game = {
+    playerMove: '',
+    computerMove: '',
   }
 
-  function printMessage(msg) {
-    let div = document.createElement('div');
-    div.innerHTML = msg;
-    document.querySelector('.messages').appendChild(div);
+
+  const hands = [...document.querySelectorAll('.select img')];
+
+  function handSelection() {
+    game.playerMove = this.dataset.option;
+    hands.forEach(hand => hand.style.boxShadow = '');
+    this.style.boxShadow = '0 0 0 4px green';
   }
 
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  let playerMove = getMoveName(playerInput);
-  let computerMove = getMoveName(randomNumber);
 
-  function getMoveName(playerMove, computerMove) {
-
-    if (computerMove == 1) {
-      return 'kamień';
+  function playGame(playerInput) {
+    if (!game.playerMove) {
+      return alert('Wybierz dłoń!!!')
     }
-    else if (computerMove == 2) {
-      return 'papier';
-    } else if (computerMove == 3) {
-      return 'nożyce';
+
+    function printMessage(msg) {
+      let div = document.createElement('div');
+      div.innerHTML = msg;
+      document.querySelector('.messages').appendChild(div);
     }
-    if (playerMove == 1) {
-      return 'kamień';
-    } else if (playerMove == 2) {
-      return 'papier';
-    } else if (playerMove == 3) {
-      return 'nożyce';
+
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    let playerMove = getMoveName(playerInput);
+    let computerMove = getMoveName(randomNumber);
+
+    function getMoveName(playerMove, computerMove) {
+
+      if (computerMove == 1) {
+        return 'kamień';
+      }
+      else if (computerMove == 2) {
+        return 'papier';
+      } else if (computerMove == 3) {
+        return 'nożyce';
+      }
+      if (playerMove == 1) {
+        return 'kamień';
+      } else if (playerMove == 2) {
+        return 'papier';
+      } else if (playerMove == 3) {
+        return 'nożyce';
+      }
     }
+    printMessage('Mój ruch to: ' + computerMove);
+
+    printMessage('Twój ruch to: ' + playerMove);
+
+    function displayResult(argPlayerMove, argComputerMove) {
+      if (argPlayerMove === argComputerMove) {
+        printMessage('Remis')
+      } else if ((argPlayerMove === 'papier' && argComputerMove === 'kamień') || (argPlayerMove === 'kamień' && argComputerMove === 'nożyce') || (argPlayerMove === 'nożyce' && argComputerMove === 'papier')) {
+        printMessage('Wygrales')
+      } else printMessage('Przegrales')
+    }
+
+    displayResult(playerMove, computerMove);
+
   }
-  printMessage('Mój ruch to: ' + computerMove);
-
-  printMessage('Twój ruch to: ' + playerMove);
-
-  function displayResult(argPlayerMove, argComputerMove) {
-    if (argPlayerMove === argComputerMove) {
-      printMessage('Remis')
-    } else if ((argPlayerMove === 'papier' && argComputerMove === 'kamień') || (argPlayerMove === 'kamień' && argComputerMove === 'nożyce') || (argPlayerMove === 'nożyce' && argComputerMove === 'papier')) {
-      printMessage('Wygrales')
-    } else printMessage('Przegrales')
+  function clearMessages() {
+    document.querySelector('.messages').innerHTML = '';
   }
 
-  displayResult(playerMove, computerMove);
-
+  hands.forEach(hand => hand.addEventListener('click', handSelection))
+  document.querySelector('.play').addEventListener('click', playGame)
+  document.querySelector('.reset').addEventListener('click', clearMessages)
 }
-function clearMessages() {
-  document.querySelector('.messages').innerHTML = '';
-}
-
-hands.forEach(hand => hand.addEventListener('click', handSelection))
-document.querySelector('.play').addEventListener('click', playGame)
-document.querySelector('.reset').addEventListener('click', clearMessages) 
