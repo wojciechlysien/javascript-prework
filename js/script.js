bd4030229d8426f22b7d3102febc441b9cd43d72
Wojciech Lysien <wojciechlysien@gmail.com>
2019-11-10 10:26:01 +0000

    problems msg

--- js/script.js
+++ js/script.js
@@ -1,73 +1,55 @@
+function playGame(playerInput) {
 
-{
-  const game = {
-    playerMove: '',
-    computerMove: '',
+  function printMessage(msg) {
+    var div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
   }
 
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let playerMove = getMoveName(playerInput);
+  let computerMove = getMoveName(randomNumber);
 
-  const hands = [...document.querySelectorAll('.select img')];
-
-  function handSelection() {
-    game.playerMove = this.dataset.option;
-    hands.forEach(hand => hand.style.boxShadow = '');
-    this.style.boxShadow = '0 0 0 4px green';
+  function getMoveName(playerMove, computerMove) {
+    if (computerMove == 1) {
+      return 'kamień';
+    }
+    else if (computerMove == 2) {
+      return 'papier';
+    } else if (computerMove == 3) {
+      return 'nożyce';
+    }
+    if (playerMove == 1) {
+      return 'kamień';
+    } else if (playerMove == 2) {
+      return 'papier';
+    } else if (playerMove == 3) {
+      return 'nożyce';
+    }
   }
 
-
-  function playGame(playerInput) {
-    if (!game.playerMove) {
-      return alert('Wybierz dłoń!!!')
-    }
-
-    function printMessage(msg) {
-      let div = document.createElement('div');
-      div.innerHTML = msg;
-      document.querySelector('.messages').appendChild(div);
-    }
-
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    let playerMove = getMoveName(playerInput);
-    let computerMove = getMoveName(randomNumber);
-
-    function getMoveName(playerMove, computerMove) {
-
-      if (computerMove == 1) {
-        return 'kamień';
-      }
-      else if (computerMove == 2) {
-        return 'papier';
-      } else if (computerMove == 3) {
-        return 'nożyce';
-      }
-      if (playerMove == 1) {
-        return 'kamień';
-      } else if (playerMove == 2) {
-        return 'papier';
-      } else if (playerMove == 3) {
-        return 'nożyce';
-      }
-    }
-    printMessage('Mój ruch to: ' + computerMove);
-
-    printMessage('Twój ruch to: ' + playerMove);
-
-    function displayResult(argPlayerMove, argComputerMove) {
-      if (argPlayerMove === argComputerMove) {
-        printMessage('Remis')
-      } else if ((argPlayerMove === 'papier' && argComputerMove === 'kamień') || (argPlayerMove === 'kamień' && argComputerMove === 'nożyce') || (argPlayerMove === 'nożyce' && argComputerMove === 'papier')) {
-        printMessage('Wygrales')
-      } else printMessage('Przegrales')
-    }
-
-    displayResult(playerMove, computerMove);
-
+  function displayResult(argPlayerMove, argComputerMove) {
+    if (argPlayerMove === argComputerMove) {
+      printMessage('Remis')
+    } else if ((argPlayerMove === 'papier' && argComputerMove === 'kamień') || (argPlayerMove === 'kamień' && argComputerMove === 'nożyce') || (argPlayerMove === 'nożyce' && argComputerMove === 'papier')) {
+      printMessage('Wygrales')
+    } else printMessage('Przegrales')
   }
+  displayResult(playerMove, computerMove);
+
   function clearMessages() {
-    document.querySelector('.messages').innerHTML = '';
+    document.getElementById('messages').innerHTML = '';
   }
+  // let testReset = document.getElementById('reset');
 
-  hands.forEach(hand => hand.addEventListener('click', handSelection))
-  document.querySelector('.play').addEventListener('click', playGame)
-  document.querySelector('.reset').addEventListener('click', clearMessages)
+  testReset.document.getElementById('reset').addEventListener('click', clearMessages);
+
+  document.getElementById('papier').addEventListener('click', playGame);
+  document.getElementById('kamień').addEventListener('click', playGame);
+  document.getElementById('nożyce').addEventListener('click', playGame);
 }
+
+
+
+
+
