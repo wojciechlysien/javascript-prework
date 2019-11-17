43c8d2f508508be59467af622629bce44b85d1b2
Wojciech Lysien <wojciechlysien@gmail.com>
2019-11-17 16:25:08 +0000

    fixed code

--- js/script.js
+++ js/script.js
@@ -1,33 +1,23 @@
 const playGame = function (playerInput) {
-
   function printMessage(msg) {
     var div = document.createElement('div');
     div.innerHTML = msg;
     document.getElementById('messages').appendChild(div);
   }
 
+  const getMoveName = function (move) {
+    if (move === 1) {
+      return 'kamień';
+    } else if (move === 2) {
+      return 'papier';
+    } else if (move === 3) {
+      return 'nożyce';
+    }
+  };
+
   let randomNumber = Math.floor(Math.random() * 3 + 1);
   let playerMove = getMoveName(playerInput);
   let computerMove = getMoveName(randomNumber);
-
-  const getMoveName = function (playerMove, computerMove) {
-
-    if (computerMove == 1) {
-      return 'kamień';
-    }
-    else if (computerMove == 2) {
-      return 'papier';
-    } else if (computerMove == 3) {
-      return 'nożyce';
-    }
-    if (playerMove == 1) {
-      return 'kamień';
-    } else if (playerMove == 2) {
-      return 'papier';
-    } else if (playerMove == 3) {
-      return 'nożyce';
-    }
-  }
   const displayResult = function (argPlayerMove, argComputerMove) {
 
     if (argPlayerMove === argComputerMove) {
@@ -35,22 +25,15 @@ const playGame = function (playerInput) {
     } else if ((argPlayerMove === 'papier' && argComputerMove === 'kamień') || (argPlayerMove === 'kamień' && argComputerMove === 'nożyce') || (argPlayerMove === 'nożyce' && argComputerMove === 'papier')) {
       printMessage('Wygrales')
     } else printMessage('Przegrales')
-  }
+  };
   displayResult(playerMove, computerMove);
 
-  function clearMessages() {
-    document.getElementById('messages').innerHTML = '';
-  }
-  // let testReset = document.getElementById('reset');
+};
 
-  testReset.document.getElementById('reset').addEventListener('click', clearMessages);
-
-  document.getElementById('papier').addEventListener('click', playGame);
-  document.getElementById('kamień').addEventListener('click', playGame);
-  document.getElementById('nożyce').addEventListener('click', playGame);
-}
-
-
-
-
+document.getElementById('reset').addEventListener('click', () => {
+  document.getElementById('messages').innerHTML = '';
+});
+document.getElementById('papier').addEventListener('click', () => { playGame(1) });
+document.getElementById('kamień').addEventListener('click', () => { playGame(2) });
+document.getElementById('nożyce').addEventListener('click', () => { playGame(3) });
 
